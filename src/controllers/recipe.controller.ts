a3592682647b73530dc9bccb771f5a1f0c53cd9f
Narayan Pradhan <narayanp@mindfiresolutions.com>
2025-08-04 19:22:26 +0530

    fix: update recipe controller

--- src/controllers/recipe.controller.ts
+++ src/controllers/recipe.controller.ts
@@ -22,7 +22,7 @@ export interface RecipeQuery {
  */
 export const getRecipes = async (
   req: Request<unknown, unknown, unknown, RecipeQuery>,
-  res: Response
+  res: Response,
 ) => {
   const {
     ingredients,
@@ -61,7 +61,7 @@ export const getRecipes = async (
     parsedLimit,
     skip,
     sortBy,
-    parsedOrder
+    parsedOrder,
   );
 
   res.json({ success: true, recipes });
@@ -87,7 +87,7 @@ export const getRecipeById = asyncHandler(
       res.status(404).json({ message: "didn't find the recipe" });
       return;
     }
-  }
+  },
 );
 
 /**
@@ -128,7 +128,7 @@ export const createRecipe = asyncHandler(
     const recipe = await recipeService.createNewRecipe(validatedData);
 
     res.status(201).json({ recipe });
-  }
+  },
 );
 
 /**
@@ -191,7 +191,7 @@ export const deleteRecipe = asyncHandler(
     }
     await recipeService.removeRecipe(req.params.id);
     res.status(204).end();
-  }
+  },
 );
 
 /**
@@ -204,5 +204,5 @@ export const getMyRecipes = asyncHandler(
     const userId = (req as AuthRequest).user.id;
     const myRecipies = await recipeService.getMyRecipes(userId);
     res.status(200).json({ myRecipies });
-  }
+  },
 );
